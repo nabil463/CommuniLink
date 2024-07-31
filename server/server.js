e1ba872cb929a459bbb89ab21f0db91791ca4279
Mohammed Nabil <mohammad.nabil2974@gmail.com>
2024-07-31 16:48:25 +0400

    Feat: Signup Functionality Completed

--- server/server.js
+++ server/server.js
@@ -2,17 +2,22 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
-dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
-});
+dotenv.config({path: "../.env"});
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+
+// app.get('/', (req, res) => {
+//     res.send("Hello World!");
+// });
 
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, ()=>{
+    connectToMongoDB();
     console.log(`Server is active on port ${PORT}`)
-})
+}) 
