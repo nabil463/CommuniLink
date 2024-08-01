bbc00a892434635bb7dfbec92b84445aac4f32c3
Mohammed Nabil <mohammad.nabil2974@gmail.com>
2024-08-01 11:46:50 +0400

    Feat: getMessages controller added

--- server/contollers/message.controllers.js
+++ server/contollers/message.controllers.js
@@ -42,4 +42,21 @@ export const sendMessage = async (req,res) => {
     }
 };
 
-export default sendMessage;
+export const getMessages = async (req,res) => {
+    try {
+        const {id:userToChatId} = req.params;
+        const senderId = req.user._id;
+
+        const conversation = await Conversation.findOne({
+            participants:{ $all: [senderId, userToChatId]},
+        }).populate("messages");
+
+        if(!conversation) return res.status(200).json([]);
+        const messages = conversation.messages;
+
+        res.status(200).json(messages);
+    } catch (error) {
+        console.log("Error in getMessages controller: ", error.message);
+        res.status(500).json({error:"Internal Server Error"});
+    }
+}
