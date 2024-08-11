e8ff72df45864834ba34f3aaf0292b625a6b7862
Mohammed Nabil <mohammad.nabil2974@gmail.com>
2024-08-11 10:48:04 +0530

    Feat: Client Side UI Added

---
+++ client/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Conversation from './Conversation'
+
+const Conversations = () => {
+  return (
+    <div className='py-2 flex flex-col overflow-auto '>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+    </div>
+  )
+}
+
+export default Conversations
+
+
+//Starter code snippet
+// import React from 'react'
+// import Conversation from './Conversation'
+
+// const Conversations = () => {
+//   return (
+//     <div className='py-2 flex flex-col overflow-auto '>
+//         <Conversation/>
+//         <Conversation/>
+//         <Conversation/>
+//         <Conversation/>
+//         <Conversation/>
+//         <Conversation/>
+//     </div>
+//   )
+// }
+
+// export default Conversations
